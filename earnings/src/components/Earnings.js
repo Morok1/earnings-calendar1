9fe946a4e00e746dead679cbf6d75895707611a1
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2020-04-20 18:33:36 +0300

    add routing

---
+++ earnings/src/components/Earnings.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Login from './Login';
+
+function Earnings() {
+  return (
+    <div className="App">
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            Earnings
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Login />
+    </div>
+  );
+}
+
+export default Earnings;
--- springboot/Hands-On-Full-Stack-Development-with-Spring-Boot-2-and-React-Second-Edition-master/Chapter14/src/ReactRouter/App.js
+++ springboot/Hands-On-Full-Stack-Development-with-Spring-Boot-2-and-React-Second-Edition-master/Chapter14/src/ReactRouter/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 import Home from './Home';
-import Earnings from "./components/Earnings";
+import Earnings from "../../../../../earnings/src/Earnings";
 
 function App() {
   return (
@@ -10,13 +10,11 @@ function App() {
       <BrowserRouter>
         <div>
           <Link to="/">Home</Link>{' '}
-          <Link to="/earnings">Earnings</Link>{' '}
-          <Link to="/extra">Extra</Link>{' '}
+          <Link to="/earnings">Contact</Link>{' '}
           <Link to="/links">Links</Link>{' '}
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/earnings" component={Earnings} />
-            <Route path="/extra" component={Earnings} />
             <Route path="/links" render={() => <h1>Links</h1>} />
             <Route render={() => <h1>Page not found</h1>} />
           </Switch>
